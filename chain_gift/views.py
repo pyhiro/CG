87ff9f1ba32d5822aa000074be81d2c06cf5ce96
bbb <tky1902056@stu.o-hara.ac.jp>
2020-11-17 11:19:21 +0900

    show message

--- chain_gift/views.py
+++ chain_gift/views.py
@@ -6,7 +6,7 @@ from django.http.response import JsonResponse
 from .forms import LoginForm, SignUpForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from .models import User, Secret
+from .models import User, Secret, Message
 from django.contrib.auth.decorators import login_required
 
 import datetime
@@ -150,11 +150,15 @@ def calculate_amount(request):
         return JsonResponse({'message': 'fail', 'error': response.content}, status=400)
 
 
-
-
-
+@login_required
 def message(request):
-    return render(request, 'message.html')
+    user = request.user
+    student_id = user.student_id
+    received_message = Message.objects.filter(recipient=student_id)
+    send_message = Message.objects.filter(sender=student_id)
+    params = {'receive': received_message,
+              'send': send_message}
+    return render(request, 'message.html', params)
 
 
 def point(request):
