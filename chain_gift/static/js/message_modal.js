8f6fb5a9f4e161922cadffab6125d4a43fbbd51b
IA-4941 <sinton204@yahoo.co.jp>
2021-01-12 10:17:42 +0900

    modify message modal

--- chain_gift/static/js/message_modal.js
+++ chain_gift/static/js/message_modal.js
@@ -1,4 +1,3 @@
-
 (function($) {
 	$.fn.layerBoard = function(option) {
 		var elements = this;
@@ -9,51 +8,75 @@
 				alpha : 0.8,		//背景レイヤーの透明度
 				limitMin : 0,		//何分経過後に再度表示するか/分（0で再表示なし）
 				easing: 'linear',		//イージング
-			}, option);
+            }, option);
+            
+            //スクロールポジションの変数宣言
+            var scrollPosition;
 
 			// モーダル表示用ボタンの表示処理 --------------------
-			$('.image-popup').click(function() {
+			$('#modal1,#modal2,#modal3').click(function() {
+                //スクロールポジションを取得
+                scrollPosition = $(window).scrollTop();
+                //取得したポジションで固定
+		        $('body').addClass('fixed').css({'top': -scrollPosition});
+                
                 var url = $(this).attr('name');
+
+                var i = $(this).attr('id')
+
+
                 $(this).css('background', 'rgb(230, 230, 230)')
+
 				//Ajaxを開始
 				$.ajax({
 					url: url,
 					type: 'GET',
 					dataType: 'json',
 					success: function(data) {
-					  var datum = data
+
+                        var datum = data
                         var contents = datum.contents
-                        console.log(contents)
                         var sender = datum.sender
+                        var recipient = datum.recipient
                         var point = datum.point
                         var time = datum.time
+                        //日本時間に変換
+                        var jp_time =  new Date(time).toLocaleString("ja").slice(0,-3);;;
+
                         $("#message").text(contents)
-                        $("#sender").text(sender)
+                        if(i == 'modal3'){
+                            $("#sender_recipient").text(recipient)
+                        }else{
+                            $("#sender_recipient").text(sender)
+                        }
                         $("#point").text(point+"ポイント")
-                        $("#time").text(time)
-                        
+                        $("#time").text(jp_time)
 					},
 					error: function(){
 						alert('失敗');
 					}
-				})
-				$('#layer_board_area').css('display', 'block');
-				$('.layer_board_bg', elements).show().animate({opacity: 0},0).delay(option.delayTime).animate({opacity: option.alpha},option.fadeTime,function(){
+                })
+				    $('#layer_board_area').css('display', 'block');
+				    $('.layer_board_bg', elements).show().animate({opacity: 0},0).delay(option.delayTime).animate({opacity: option.alpha},option.fadeTime,function(){
 					$('.layer_board', elements).fadeIn(option.fadeTime);
-					
-					//表示した際背景のスクロール禁止
-					$('html, body').css('overflow', 'hidden');	
-				});
+                    //<a>タグのリンクを無効化　＊これが無いとTOPに飛ぶ＊
+                    return false	
+                });
 			});
 
 			// .mdl_btn_closeをクリックした時の動作(非表示処理) --------------------
 			$('.mdl_btn_close', elements).click(function() {
 
+                //上で固定したのを無効化
+                $('body').removeClass('fixed').css({'top': 0});
+		        window.scrollTo( 0 , scrollPosition );
+
 				$('.layer_board , #layer_board_area', elements).fadeOut(option.fadeTime);
 				$('.layer_board_bg', elements).fadeOut(option.fadeTime);
 				$('#layer_board_area').css('display', 'none');
-				//非表示にした際背景のスクロール許可
-                $('html, body').css('overflow', 'auto');
+
+                //<a>タグのリンクを無効化　＊これが無いとTOPに飛ぶ
+                return false
 			});
 
 			// 見た目処理(コンテンツが短い場合中央表示) --------------------
