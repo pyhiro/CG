13612dc16d8505c92f63cbbbbd3e9944db607108
IA-4941 <sinton204@yahoo.co.jp>
2021-01-27 12:41:36 +0900

    shop

--- chain_gift/static/js/shop.js
+++ chain_gift/static/js/shop.js
@@ -2,11 +2,30 @@
 Vue.filter('number_format', function(val) {
   return val.toLocaleString();
 });
+
+Vue.component('open-modal',{
+  template : `
+    <div id="overlay" v-on:click="clickEvent">
+        <div id="content">
+          <p><slot></slot></p>
+          <a v-on:click="clickEvent">close</a>
+        </div>
+    </div>
+    `,
+  methods :{
+    clickEvent: function(){
+      this.$emit('from-child')
+    }
+  }
+})
+
+
 var app = new Vue({
+  
   delimiters: ['[[',']]'],
   el: '#app',
   data: {
-      //「並び替え」の選択値（1：標準、2：ポイントが低い順)
+      //「並び替え」の選択値（1：標準、2：ポイントが低い順、3:ポイントが高い順、4:新着順)
       sortOrder: 1,
       // 商品リスト
       products: [],
@@ -16,6 +35,14 @@ var app = new Vue({
       message: '',
       // カテゴリーの選択値　(0:全商品、1:スタンプ、2:参考書、3:文房具)
       category: 'すべての商品',
+      // モーダルの表示フラグ
+      showContent: false,
+      // モーダル表示用の名前
+      inModal_name:'',
+      // モーダル
+      inModal_Image:'guuu2.png',
+
+      isCssFlag:false
   },
   created: function() {
       // JSONPのURL（サーバーに配置する）
@@ -31,8 +58,7 @@ var app = new Vue({
       }.bind(this))
       .fail(function(jqXHR, textStatus, errorThrown) {
         this.isError = true;
-        this.message = '商品リストの読み込みに失敗しました。';
-        alert('接続失敗')
+        this.message = '商品の読み込みに失敗しました。しばらく時間を置き再度アクセスしてください。';
       }.bind(this));
   },
   methods: {
@@ -79,14 +105,40 @@ var app = new Vue({
       if (this.sortOrder == 1) {
         //元の順番に並び替え済み
       }
+      //ポイントが低い順に並び変える
       else if(this.sortOrder == 2) {
-        //ポイントが低い順に並び変える
         newList.sort(function(a,b){
           return a.goods_value - b.goods_value;
         });
       }
+      //ポイントが高い順に並び替える
+      else if(this.sortOrder == 3) {
+        newList.sort(function(a,b){
+          return b.goods_value - a.goods_value;
+        })
+      }
+      //新着順に並び替える
+      else if(this.sortOrder == 4) {
+        newList.sort(function(a,b){
+          return b.goods_id - a.goods_id;
+        })
+      }
       //絞り込み後の商品リストを返す
       return newList;
-    }
+    },
+    //モーダル表示用及びモーダルの情報を挿入
+    openModal: function(n,i){
+      this.showContent = true
+      this.inModal_name = n
+      //this.inModal_Image = i
+      this.isCssFlag = true
+
+      overlay.css('display', 'block');
+    },
+    //モーダル非表示
+    closeModal: function(){
+      this.showContent = false
+      this.inModal_name = ''
+    },
   }
 });
