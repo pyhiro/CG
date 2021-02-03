dd65437d9ad0a030f9e25481a6c723aa4dfe17f0
IA-4941 <sinton204@yahoo.co.jp>
2021-02-03 10:54:18 +0900

    shopです。

--- chain_gift/static/js/shop.js
+++ chain_gift/static/js/shop.js
@@ -2,11 +2,30 @@
 Vue.filter('number_format', function(val) {
   return val.toLocaleString();
 });
+
+Vue.component('open-modal',{
+  template : `
+    <div id="overlay">
+        <div id="content">
+          <slot></slot>
+            <a href="http:/..." onclick="return false;" class="mdl_btn_close square_btn" v-on:click="clickEvent">close</a>
+        </div>
+    </div>
+    `,
+  methods :{
+    clickEvent: function(){
+      this.$emit('from-child')
+    }
+  }
+})
+
+
 var app = new Vue({
+  
   delimiters: ['[[',']]'],
   el: '#app',
   data: {
-      //「並び替え」の選択値（1：標準、2：ポイントが低い順)
+      //「並び替え」の選択値（1：標準、2：ポイントが低い順、3:ポイントが高い順、4:新着順)
       sortOrder: 1,
       // 商品リスト
       products: [],
@@ -16,6 +35,16 @@ var app = new Vue({
       message: '',
       // カテゴリーの選択値　(0:全商品、1:スタンプ、2:参考書、3:文房具)
       category: 'すべての商品',
+      //商品の写真が無い場合の仮写真
+      syoki_img: '20201205.png',
+      // モーダルの表示フラグ
+      showContent: false,
+      // モーダル表示用の名前
+      inModal_name:'',
+      // モーダル表示用の画像
+      inModal_Image:'',
+      // モーダル表示用のポイント
+      inModal_price:0,
   },
   created: function() {
       // JSONPのURL（サーバーに配置する）
@@ -27,18 +56,26 @@ var app = new Vue({
         dataType: 'json',        // レスポンスのデータタイプ
       })
       .done(function(data, textStatus, jqXHR) {
+        //取得した商品データをproductsに格納
         this.products =  data.goods;
+
+        //グッズの商品画像が無い場合の設定
+        for (var i=0; i < this.products.length; i++){
+          if(!this.products[i].goods_img) {
+            this.products[i].goods_img = this.syoki_img
+          }
+        }
       }.bind(this))
       .fail(function(jqXHR, textStatus, errorThrown) {
+        //データが取得出来ない場合の処理
         this.isError = true;
-        this.message = '商品リストの読み込みに失敗しました。';
-        alert('接続失敗')
+        this.message = '商品の読み込みに失敗しました。しばらく時間を置き再度アクセスしてください。';
       }.bind(this));
   },
   methods: {
+    //以下４つカテゴリーの設定
     allList: function() {
       this.category = 'すべての商品';
-
     },
     stampList: function() {
       this.category = 'スタンプ';
@@ -67,6 +104,7 @@ var app = new Vue({
           //全商品の場合
         }else if (this.category == 'すべての商品'){
           isShow = true;
+          //何かの拍子にすり抜けた場合
         }else{
           isShow = false;
         }
@@ -79,14 +117,40 @@ var app = new Vue({
       if (this.sortOrder == 1) {
         //元の順番に並び替え済み
       }
+      //ポイントが低い順に並び変える
       else if(this.sortOrder == 2) {
-        //ポイントが低い順に並び変える
         newList.sort(function(a,b){
           return a.goods_value - b.goods_value;
         });
       }
+      //ポイントが高い順に並び替える
+      else if(this.sortOrder == 3) {
+        newList.sort(function(a,b){
+          return b.goods_value - a.goods_value;
+        })
+      }
+      //新着順に並び替える
+      else if(this.sortOrder == 4) {
+        newList.sort(function(a,b){
+          return b.goods_id - a.goods_id;
+        })
+      }
       //絞り込み後の商品リストを返す
       return newList;
-    }
+    },
+    //モーダル表示用及びモーダルの情報を挿入
+    openModal: function(n,i,p){
+      this.showContent = true
+      this.inModal_name = n
+      this.inModal_Image = i
+      this.inModal_price = p
+    },
+    //モーダル非表示及び情報の初期化
+    closeModal: function(){
+      this.showContent = false
+      this.inModal_name = ''
+      this.inModal_Image = ''
+      this.inModal_price = 0
+    },
   }
 });
